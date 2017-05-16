6109fff241d31ace60eeb6e40734e00213cf0b4e
HN <nidboubkerhoussine@live.fr>
2017-05-16 13:39:37 +0200

    Modify connection string name commit

---
+++ Src/Web/www/Mona.Web/Migrations/201705161137525_MigrationStart.cs
@@ -0,0 +1,34 @@
+namespace Mona.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrationStart : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contact",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Code = c.Int(nullable: false),
+                        Picture = c.String(),
+                        ContactType = c.Int(nullable: false),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        PhoneNumber = c.String(),
+                        Email = c.String(),
+                        TwiterAddress = c.String(),
+                        FaceBookAddress = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Contact");
+        }
+    }
+}
---
+++ Src/Web/www/Mona.Web/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Mona.Web.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Mona.Web.Data.DefaultContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Mona.Web.Data.DefaultContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
