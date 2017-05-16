b4da7c121dfaf8f7eaeea321e4cf4336a23ae45a
HN <nidboubkerhoussine@live.fr>
2017-05-16 12:45:56 +0200

    Add defaulContext and config contact Mapping commit

---
+++ Src/Web/www/Mona.Web/Data/DefaultContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Mona.Web.Entities;
+
+namespace Mona.Web.Data
+{
+    public class DefaultContext : DbContext
+    {
+        public DefaultContext():base("Default")
+        {
+            
+        }
+
+        public IDbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            ContactMapping(modelBuilder);
+        }
+
+        private void ContactMapping(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Contact>()
+                .ToTable("Contact")
+                .HasKey(o => o.Id);
+
+        }
+    }
+}
