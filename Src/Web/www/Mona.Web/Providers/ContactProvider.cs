d98c255496a68d517525a93a12a53b80f07ff859
HN <nidboubkerhoussine@live.fr>
2017-05-16 15:37:07 +0200

    Start Refactoring EF contact provider

---
+++ Src/Web/www/Mona.Web/Providers/ContactProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mona.Web.Providers
+{
+    public interface IContactProvider
+    {
+    }
+
+    public class ContactProvider : IContactProvider
+    {
+    }
+}
