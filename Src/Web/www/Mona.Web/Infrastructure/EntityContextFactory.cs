d7b72684cdfce5e936d622a9fcec017452a05aab
HN <nidboubkerhoussine@live.fr>
2017-05-16 23:07:54 +0200

    Implement Context Factory commit

--- Src/Web/www/Mona.Web/Infrastructure/EntityContextFactory.cs
+++ Src/Web/www/Mona.Web/Infrastructure/EntityContextFactory.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Mona.Web.Data;
 
 namespace Mona.Web.Infrastructure
 {
-    public class EntityContextFactory
+    public interface IEntityContextFactory
     {
+        IDataContext DataContext { get; }
+        IDataContext Get();
+    }
+
+    public class EntityContextFactory : IEntityContextFactory
+    {
+        protected IDataContext Context;
+
+        public IDataContext DataContext
+        {
+            get { return Context ?? (Context = new DefaultContext()); }
+        }
+
+        public virtual IDataContext Get()
+        {
+            return Context ?? (Context = DataContext);
+        }
     }
 }
