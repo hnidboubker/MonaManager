0a74915297ee7c344f8eca842663f9ff1dbf589e
HN <nidboubkerhoussine@live.fr>
2017-05-16 19:44:26 +0200

    Create Generic EntityRepository commit

---
+++ Src/Web/www/Mona.Web/Infrastructure/EntityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mona.Web.Infrastructure
+{
+    public interface IEntityRepository<T> : IRepository<T, long>
+    {
+    }
+
+    public class EntityRepository<T> : Repository<T, long>, IEntityRepository<T>
+    {
+    }
+}
