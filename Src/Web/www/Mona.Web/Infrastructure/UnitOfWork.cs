194c0dbee214fa68ab4c97576ad966063b07d783
HN <nidboubkerhoussine@live.fr>
2017-05-16 23:21:20 +0200

    Create Unit of  work && add methods commit

---
+++ Src/Web/www/Mona.Web/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mona.Web.Data;
+
+namespace Mona.Web.Infrastructure
+{
+    public interface IUnitOfWork
+    {
+        IDataContext DataContext { get; }
+        int Commit();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        protected IDataContext Context;
+        protected IEntityContextFactory EntityContextFactory;
+
+        public UnitOfWork(IEntityContextFactory entityContextFactory)
+        {
+            EntityContextFactory = entityContextFactory;
+        }
+
+        public IDataContext DataContext
+        {
+            get { return Context ?? (Context = EntityContextFactory.Get()); }
+        }
+
+        public virtual int Commit()
+        {
+            var result = DataContext.Commit();
+            return result;
+        }
+    }
+}
