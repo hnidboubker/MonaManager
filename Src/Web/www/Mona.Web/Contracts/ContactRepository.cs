8236264830a52b2cac1108f0af6d815afc0b4c2b
HN <nidboubkerhoussine@live.fr>
2017-05-16 15:35:08 +0200

    Start Refactoring EF contact repository

---
+++ Src/Web/www/Mona.Web/Contracts/ContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mona.Web.Contracts
+{
+    public interface IContactRepository
+    {
+    }
+
+    public class ContactRepository : IContactRepository
+    {
+    }
+}
