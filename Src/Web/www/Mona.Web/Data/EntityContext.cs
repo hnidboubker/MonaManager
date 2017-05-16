04d65594bfa49180f0f623989cc12a01f2bed20b
HN <nidboubkerhoussine@live.fr>
2017-05-16 22:47:19 +0200

    Create Generic Context commit

---
+++ Src/Web/www/Mona.Web/Data/EntityContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mona.Web.Data
+{
+    public class EntityContext : DataContext<EntityContext>
+    {
+    }
+
+    public class DataContext<TContext> : DbContext where TContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<TContext>());
+        }
+    }
+}
