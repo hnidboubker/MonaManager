8ab35f1ab67ec5d332121b99985109cf77ccee93
HN <nidboubkerhoussine@live.fr>
2017-05-16 19:45:58 +0200

    Create Generic Entity Service commit

---
+++ Src/Web/www/Mona.Web/Infrastructure/EntityService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mona.Web.Infrastructure
+{
+    public interface IEntityService<T> : IService<T, long>
+    {
+    }
+
+    public class EntityService<T> : Service<T, long>, IEntityService<T>
+    {
+    }
+}
